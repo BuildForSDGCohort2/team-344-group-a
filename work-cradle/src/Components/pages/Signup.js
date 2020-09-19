b4bde4af9b93485f6b241cb78d60b1b699709b14
Joblyn <oaikhenahjob@gmail.com>
2020-09-20 00:38:41 +0100

    user auth front-end

--- work-cradle/src/Components/pages/Signup.js
+++ work-cradle/src/Components/pages/Signup.js
@@ -3,7 +3,95 @@ import { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class SignUp extends Component {
+
+  constructor(props) {
+    super(props); 
+    
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      incorrectPass: false,
+      passConfirmed: true,
+    } 
+
+    this.NameHandler = this.NameHandler.bind(this);
+    this.repeatPasswordHandler = this.repeatPasswordHandler.bind(this);
+    this.emailHandler = this.emailHandler.bind(this);
+    this.passwordHandler = this.passwordHandler.bind(this);
+    this.register = this.register.bind(this);
+  }
+
+  NameHandler(e) {
+   this.setState({
+    [e.target.name]: e.target.value
+   }) 
+  }
+
+  emailHandler(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  passwordHandler(e) { 
+    if (e.target.value.length >= 8) {
+      this.setState({
+        password: e.target.value,
+        incorrectPass: false
+      }) 
+    } else {
+      this.setState({
+        incorrectPass: true
+      })
+    }
+  }
+
+  repeatPasswordHandler(e) {
+    this.setState({
+      confirmPassword: e.target.value,
+    })  
+  }
+
+  passAlert = () => <p style={{color:'red', fontFamily:'sans-serif', paddingLeft: 20}}>Passwords don't match!</p>
+
+  register() { 
+    return (
+      !(this.state.confirmPassword === this.state.password) ?
+        this.setState({passConfirmed:false}) 
+        : () => {
+          const userDetails = {
+            firsName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password
+          } 
+          fetch('url', {
+            method: 'post',
+            body: JSON.stringify(
+              userDetails
+            ), 
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            } 
+          }) 
+          .then(response => {
+            response.json().then(resp => {
+              console.log(resp)
+            })
+          })
+        } 
+
+    )
+
+  }
+
   render() {
+    const { incorrectPass, passConfirmed } = this.state;
+
     return(
       <>
       <div id="titlebar" className="gradient">
@@ -53,30 +141,64 @@ class SignUp extends Component {
               {/* <!-- Form --> */}
               <form method="post" id="register-account-form">
                 <div className="input-with-icon-left">
+                  {/* <i className="icon-material-baseline-mail-outline"></i> */}
+                  <input 
+                  type="text" 
+                  className="input-text with-border" 
+                  name="firstName" 
+                  id="fist-name" 
+                  placeholder="First Name"  
+                  onChange={this.NameHandler}
+                  required/>
+                </div>
+                <div className="input-with-icon-left">
+                  {/* <i className="icon-material-baseline-mail-outline"></i> */}
+                  <input 
+                  type="text" 
+                  className="input-text with-border" 
+                  name="lastName" 
+                  id="last-name" 
+                  placeholder="last Name"  
+                  onChange={this.NameHandler}
+                  required/>
+                </div>
+                <div className="input-with-icon-left">
                   <i className="icon-material-baseline-mail-outline"></i>
-                  <input type="text" className="input-text with-border" name="emailaddress-register" id="emailaddress-register" placeholder="Email Address" required/>
+                  <input 
+                  type="email" 
+                  className="input-text with-border" 
+                  name="email-register" 
+                  id="email-register" 
+                  placeholder="Email Address"  
+                  onChange={this.emailHandler}
+                  required/>
                 </div>
 
                 <div className="input-with-icon-left" title="Should be at least 8 characters long" data-tippy-placement="bottom">
                   <i className="icon-material-outline-lock"></i>
-                  <input type="password" className="input-text with-border" name="password-register" id="password-register" placeholder="Password" required/>
+                  <input type="password" className="input-text with-border" name="password-register" id="password-register" placeholder="Password" required onChange={this.passwordHandler}
+                  style = {incorrectPass ? {border: '1px solid red'} : null}
+                  /> 
                 </div>
 
                 <div className="input-with-icon-left">
                   <i className="icon-material-outline-lock"></i>
-                  <input type="password" className="input-text with-border" name="password-repeat-register" id="password-repeat-register" placeholder="Repeat Password" required/>
+                  <input type="password" className="input-text with-border" name="password-repeat-register" id="password-repeat-register" placeholder="Repeat Password" required onChange={this.repeatPasswordHandler}
+                  /> 
+                  { !passConfirmed ? this.passAlert() : '' }
                 </div>
+                
               </form>
               
               {/* <!-- Button --> */}
-              <button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit" form="login-form">Register <i className="icon-material-outline-arrow-right-alt"></i></button>
+              <button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit" form="login-form" onClick={this.register}>Register <i className="icon-material-outline-arrow-right-alt"></i></button>
               
               {/* <!-- Social Login --> */}
-              <div className="social-login-separator"><span>or</span></div>
+              {/* <div className="social-login-separator"><span>or</span></div>
               <div className="social-login-buttons">
                 <button className="facebook-login ripple-effect"><i className="icon-brand-facebook-f"></i> Register via Facebook</button>
                 <button className="google-login ripple-effect"><i className="icon-brand-google-plus-g"></i> Register via Google+</button>
-              </div>
+              </div> */}
             </div>
 
           </div>
